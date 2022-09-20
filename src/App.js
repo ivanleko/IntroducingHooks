5d1e9eefc25e33a3eb4ecf4cce8bf4c0ea2b5087
Ivan Leko <ivanleko@Ivans-MacBook-Pro.local>
2022-09-20 09:46:11 +0200

    introducinghooks initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useReducer } from 'react';
+import { Button } from 'bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function reducer(state,action){
+  switch (type, action){
+    case "increment": return state + 1
+    case "decrement": return state - 1
+  }
+}
+function App (){
+  const [state, dispatch] = useReducer(reducer, 0)
+  return(
+    <Button onClick={()=> dispatch({type: "increment"})} > + </Button>,
+    <Button onClick={()=> dispatch({type: "decrement"})} > - </Button>
   );
 }
 
